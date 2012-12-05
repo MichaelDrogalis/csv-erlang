fbdb2504965f75d12509730cb332b67f7856a767
Michael Drogalis <mjd3089@rit.edu>
2012-12-04 19:44:35 -0500

    Step 3 complete. Added some notes about my thoughts as I go along.

--- csv.erl
+++ csv.erl
@@ -1,6 +1,9 @@
 -module(csv).
--export([parse_csv/1]).
+-export([parse_csv/1, parse/1]).
 
 parse_csv (Line) ->
     Lines = re:split(Line, ",", [{return, list}]).
+
+parse (Lines) ->
+    lists:map (fun (Line) -> parse_csv (Line) end, Lines).
 
