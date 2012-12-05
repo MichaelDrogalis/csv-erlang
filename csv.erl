0a8f669d853c50799f5c0aa0a748d6823feadf09
Michael Drogalis <mjd3089@rit.edu>
2012-12-04 20:06:33 -0500

    Step 5 complete.

--- csv.erl
+++ csv.erl
@@ -8,5 +8,5 @@ parse (Lines) ->
     lists:map (fun (Line) -> parse_csv (Line) end, Lines).
 
 process (Lines, F) ->
-    lists:map (fun (Tokens) -> F (Tokens) end, parse (Lines)).
+    lists:map (fun (Tokens) -> F (list_to_tuple(Tokens)) end, parse (Lines)).
 
