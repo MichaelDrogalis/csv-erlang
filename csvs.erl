e375e7b4c78d696822a6d34a9a857679188839de
Michael Drogalis <mjd3089@rit.edu>
2012-12-05 21:15:42 -0500

    Step 3 of concurrency.

--- csvs.erl
+++ csvs.erl
@@ -1,9 +1,33 @@
 -module(csvs).
--export([open/1]).
-
-some_func(X) ->
-    X.
+-export([open/1, service_loop/1, test_connection/0]).
 
 open (FileName) ->
-    spawn (csv, process_file, [FileName, fun some_func/1]).
+    Lines = csv:process_file (FileName, fun identity/1),
+    spawn (csvs, service_loop, [Lines]).
+
+identity (X) ->
+    X.
+
+service_loop ([]) ->
+    receive
+        {next_record, Pid} -> Pid ! {ok, eof}
+    end;
+service_loop (Lines) ->
+    receive
+        {next_record, Pid} ->
+                    Pid ! {ok, hd(Lines)},
+                    service_loop(tl(Lines));
+        {close, Pid} -> Pid ! {ok, closed};
+        {_, Pid} -> Pid ! {error, bad_request}
+    end.
+
+test_connection () ->
+    Pid = open("file.csv"),
+    Pid ! {closex, self()},
+    receive
+        {ok, eof} -> io:format("Aaaand we're done.~n");
+        {ok, closed} -> io:format("Closed.~n");
+        {ok, Tuple} -> io:format("~p ~n", [Tuple]);
+        {error, bad_request} -> io:format("Bad request..~n")
+    end.
 
